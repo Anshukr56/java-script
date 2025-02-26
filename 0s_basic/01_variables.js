c730e34d57c4c02bcceb22826ae77e76e072d37f
Anshukr56 <anshukr552005@gmail.com>
2025-02-26 09:08:33 +0000

    variable

---
+++ 0s_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453 //const is fixed it cant be changer but let can be changed
+let accountEmail = "anshukr552005@gmail.com"
+var accountPassword = "12345"  //dont use var use only let 
+accountCity = "bengaluru"
+//accountId = 2 //not alloweed
+acountEmail = "anshu@gmail.com"
+accountPassword = "23424"
+accountCity = "patna"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
