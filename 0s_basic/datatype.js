aa87365fbb528cf697db7f63c2b18eac5d88f6ef
Anshukr56 <anshukr552005@gmail.com>
2025-03-05 14:18:49 +0000

    datatype

---
+++ 0s_basic/datatype.js
@@ -0,0 +1,28 @@
+// primitive datatype
+// 7 types : string,number,boolean,null,undefined,symbol,bigInt
+
+const score = 200;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = symbol('123')
+const anotherId=symbol('123')
+//console.log(id === anotherId)
+//reference(non primitive)
+
+//array,object,function
+
+const heros = ["ironman","superman"]
+let myObj ={
+    name:"Anshu",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log('Hello world');
+}
+
+console.log(typeof bigNumber);
