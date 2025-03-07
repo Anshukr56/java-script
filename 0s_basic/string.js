a19f8949c76a581f5e7e07c2e982f0404375bfb6
Anshukr56 <anshukr552005@gmail.com>
2025-03-07 15:21:39 +0000

    string

---
+++ 0s_basic/string.js
@@ -0,0 +1,15 @@
+const name = "anshu";
+const repoCount = 50;
+
+//console.log(name + repoCount+"value");
+//dont use this method to print
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new string('anshukr')
+
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+
+
