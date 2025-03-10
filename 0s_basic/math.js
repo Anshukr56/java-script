670f743a7029372c0b37ee2f72f790a61419da3b
Anshukr56 <anshukr552005@gmail.com>
2025-03-10 16:00:05 +0000

    math

---
+++ 0s_basic/math.js
@@ -0,0 +1,15 @@
+const score = 400;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length); // Fixed method name
+
+const otherNumber = 23.4545;
+//console.log(otherNumber.toPrecision(3)); // Correct usage of toPrecision()
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.5));
