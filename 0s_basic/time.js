100c77fd5df54771ae5354dda6cc99f997df8706
Anshukr56 <anshukr552005@gmail.com>
2025-03-11 15:22:55 +0000

    time

---
+++ 0s_basic/time.js
@@ -0,0 +1,7 @@
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocalString());
+
+let myCreatedDate = new Date(2025, 0, 11)
+console.log(myCreatedDate.toDateString())
