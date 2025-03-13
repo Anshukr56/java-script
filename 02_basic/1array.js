5b461c351563792f0a4a8d74453c5851a8b0c5d8
Anshukr56 <anshukr552005@gmail.com>
2025-03-13 14:48:08 +0000

    array

---
+++ 02_basic/1array.js
@@ -0,0 +1,10 @@
+const myArr = [0,1,2,3,4,5]
+const myHeors = ["ironman","batman"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+
+//array method
+//myArr.push(6)
+console.log(myArr);
