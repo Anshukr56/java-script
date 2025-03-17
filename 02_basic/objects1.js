603e7c9c08ab1b3367c689d25153ddfb8fcf84ec
Anshukr56 <anshukr552005@gmail.com>
2025-03-17 14:40:38 +0000

    objects1

---
+++ 02_basic/objects1.js
@@ -0,0 +1,13 @@
+const course = {
+    coursename : "js in hindi ",
+    price : "999",
+    courseIns : "anshu"
+}
+
+const{courseIns} = course
+console.log(courseIns);
+
+
+{
+    name : "anshu"
+}
