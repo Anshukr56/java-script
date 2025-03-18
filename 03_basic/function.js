40d88673638fc0f98f300e233515e8bd558d955d
Anshukr56 <anshukr552005@gmail.com>
2025-03-18 14:53:10 +0000

    function

---
+++ 03_basic/function.js
@@ -0,0 +1,21 @@
+function sayMyName(){
+    console.log("A");
+    console.log("N");
+    console.log("S");
+    console.log("H");
+    console.log("U");
+}
+//sayMyName()
+
+function addTwoNumber(number1,number2){
+    // console.log(number1+number2);
+}
+
+addTwoNumber(3,4)
+
+
+function loginUserMessage(udername){
+    return '$(username) just logged in '
+}
+
+console.log(loginUserMessage("anshu"))
