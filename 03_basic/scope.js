62febbe0b8557bea1d47a2af35e7107675e266af
Anshukr56 <anshukr552005@gmail.com>
2025-03-21 16:28:19 +0000

    scope

---
+++ 03_basic/scope.js
@@ -0,0 +1,30 @@
+let a = 10
+const b  = 20
+var c = 30
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username = "anshu"
+
+    function two(){
+        const website = "youtube"
+       // console.log(website);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+
+if(true){
+   const username = "anshu"
+   if(username === "anshu"){
+    const website = "youtube"
+    console.log(username+website);
+   }
+   console.log(website);
+}
+console.log(username);
